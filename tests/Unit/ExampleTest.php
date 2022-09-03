0b90fc1efbd32e0b14d0810a75837a35561af584
Cristian Vega <eduagarcia75@gmail.com>
2022-09-03 13:25:04 -0500

    Added CI

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
